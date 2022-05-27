ec7b0a8c457c53f39a8b5d7448cf31af3c360d22
MickelsonKlaus <klausmickelson52@email.com>
2022-05-27 21:50:59 +0100

    first preview

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -19,6 +19,7 @@ export default function NavBar() {
               src="/assets/images/logo.png"
               alt="titta digital"
               layout="fill"
+              priority
             />
           </a>
         </Link>
